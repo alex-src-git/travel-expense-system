e881d13d0bf6767bd7953606a989824d481d32be
Alex <alexander.daley.me@gmail.com>
2020-04-11 11:35:38 +0100

    Store percentages as decimals, rename percentage fields to be from the employee's perspective

--- travel-expense-system/src/receipt-config.hpp
+++ travel-expense-system/src/receipt-config.hpp
@@ -2,8 +2,8 @@
 
 struct ReceiptConfig final
 {
-    double pctTravelEmployer = 100;
-    double pctExpensesEmployer = 0;
+    double pctTravelEmployee = 1.0;
+    double pctExpensesEmployee = 0;
     double bandTravelEmployee = 0;
     double bandExpensesEmployee = 50;
     char currency[4] = "GBP";
